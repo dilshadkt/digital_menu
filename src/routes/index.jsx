68c4f47d5d24f00e53961bb36a6166fe5145989c
dilshad <hmydilshadkt@gmail.com>
2025-12-18 15:57:18 +0530

    Update import path for HomePage in index.jsx to use the correct directory structure

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,7 @@ import { ProductProvider } from "../context/ProductContext";
 import { Route, Routes } from "react-router-dom";
 import ListLayout from "../layouts/ListLayout";
 import ListPage from "../pages/ListPage";
-import HomePage from "../pages/home";
+import HomePage from "../pages/home/index";
 
 const AppRoutes = () => {
   return (
