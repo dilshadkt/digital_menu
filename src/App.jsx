92431c76326e8b4564131b3e8a81e408236ffc55
dilshad <hmydilshadkt@gmail.com>
2025-12-18 15:55:08 +0530

    Refactor App component: remove unused imports and simplify structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { useState } from "react";
-import Sidebar from "./components/Sidebar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ListLayout from "./layouts/ListLayout";
-import ListPage from "./pages/ListPage";
-import { CartProvider } from "./context/CartContext";
-import { ProductProvider } from "./context/ProductContext";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 
 function App() {
